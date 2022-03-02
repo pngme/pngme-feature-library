c8bd54c84e268d1ebad0402547fde564d0175ccf
Ben Fasoli <ben@pngme.com>
2022-03-01 16:44:35 -0800

    Wrap domain logic in function

--- sum_of_balances_latest/main.py
+++ sum_of_balances_latest/main.py
@@ -1,39 +1,53 @@
+#!/usr/bin/env python3
+
 import os
 from datetime import datetime, timedelta
 
 from pngme.api import Client
 
-token = os.environ["PNGME_TOKEN"]
-client = Client(token)
+
+def get_sum_of_balances_latest(
+    client: Client, user_uuid: str, utc_starttime: datetime, utc_endtime: datetime,
+) -> float:
+    """Return the latest balance summed across all accounts."""
+    accounts = client.accounts.get(user_uuid)
+
+    # Sum the most recent (within 30 day window) balance in each account.
+    sum_of_balances_latest = 0
+    for account in accounts:
+        account_uuid = account.acct_uuid
+        balances = client.balances.get(
+            user_uuid,
+            account_uuid,
+            utc_starttime=utc_starttime,
+            utc_endtime=utc_endtime,
+        )
+
+        if balances:
+            # Sort balances descending in time, so the first is the most recent.
+            balances_sorted_by_time_descending = sorted(
+                balances,
+                key=lambda balance: balance.ts,
+                reverse=True,
+            )
+            sum_of_balances_latest += balances_sorted_by_time_descending[0].balance
+
+    return sum_of_balances_latest
 
 
-# Fetch a list of a user's financial accounts.
-user_uuid = "958a5ae8-f3a3-41d5-ae48-177fdc19e3f4"
-accounts = client.accounts.get(user_uuid)
+if __name__ == "__main__":
+    token = os.environ["PNGME_TOKEN"]
+    client = Client(token)
 
+    # Mercy Otingo, mercy@pngme.demo, 234112312
+    user_uuid = "958a5ae8-f3a3-41d5-ae48-177fdc19e3f4"
+    utc_endtime = datetime(2021, 10, 31)
+    utc_starttime = utc_endtime - timedelta(days=30)
 
-# Sum the most recent (within 30 day window) balance in each account.
-utc_endtime = datetime(2021, 10, 31)
-utc_starttime = utc_endtime - timedelta(days=30)
-
-sum_of_balances_latest = 0
-for account in accounts:
-    account_uuid = account.acct_uuid
-    balances = client.balances.get(
-        user_uuid,
-        account_uuid,
+    sum_of_balances_latest = get_sum_of_balances_latest(
+        client=client,
+        user_uuid=user_uuid,
         utc_starttime=utc_starttime,
         utc_endtime=utc_endtime,
     )
-
-    if balances:
-        # Sort balances descending in time, so the first is the most recent.
-        balances_sorted_by_time_descending = sorted(
-            balances,
-            key=lambda balance: balance.ts,
-            reverse=True,
-        )
-        sum_of_balances_latest += balances_sorted_by_time_descending[0].balance
-
-
-print(sum_of_balances_latest)
+    print(sum_of_balances_latest)
