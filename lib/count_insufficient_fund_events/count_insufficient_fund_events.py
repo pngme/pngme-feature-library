8511f48490151953ed7b47937d3e1a717648fb80
Ben Fasoli <ben@pngme.com>
2022-03-03 14:52:38 -0800

    Use institutions endpoint

--- lib/count_insufficient_fund_events/count_insufficient_fund_events.py
+++ lib/count_insufficient_fund_events/count_insufficient_fund_events.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
-"""
-Sums of insufficient fund events for a given user over all institutions.
-Typical date ranges are last 30 days, 31-60 days and 61-90 days.
-"""
-from datetime import datetime
-from datetime import timedelta
-from pngme.api import Client
 
 import os
+from datetime import datetime, timedelta
+
+from pngme.api import Client
 
 
-def count_insufficient_fund_events(
+def count_insufficient_funds_events(
     api_client: Client, user_uuid: str, utc_starttime: datetime, utc_endtime: datetime
 ) -> float:
     """
-    Calculates the sum of all events labelled with InsufficientFunds, across all institutions.
+    Count events labelled with InsufficientFunds across all institutions.
+
+    Typical date ranges are last 30 days, 31-60 days and 61-90 days.
 
     Args:
         api_client: Pngme API client
@@ -23,39 +21,43 @@ def count_insufficient_fund_events(
         utc_endtime: the datetime for the right-hand-side of the time-window
 
     Returns:
-        the sum insufficient funds events
+        count of insufficient funds events within the given time window
     """
     insufficient_funds_label = "InsufficientFunds"
 
-    account_details = api_client.accounts.get(user_uuid=user_uuid)
+    institutions = api_client.institutions.get(user_uuid=user_uuid)
 
-    # collect all alert records with a label of InsufficientFunds
-    record_list = []
-    for individual_account in account_details:
+    insufficient_funds_event_count = 0
+    for individual_account in institutions:
         insufficient_funds_events = api_client.alerts.get(
             user_uuid=user_uuid,
-            account_uuid=individual_account.acct_uuid,
+            institution_id=individual_account.institution_id,
             utc_starttime=utc_starttime,
             utc_endtime=utc_endtime,
-            labels=[insufficient_funds_label]
+            labels=[insufficient_funds_label],
         )
-        record_list.extend([dict(record) for record in insufficient_funds_events])
+        insufficient_funds_event_count += len(insufficient_funds_events)
 
-    total_event_count = len(record_list)
-    return total_event_count
+    return insufficient_funds_event_count
 
 
 if __name__ == "__main__":
-
-    USER_UUID = "958a5ae8-f3a3-41d5-ae48-177fdc19e3f4"  # Mercy Otieno, mercy@pngme.demo.com, 254123456789
-
-    client = Client(access_token=os.environ["PNGME_TOKEN"])
-
+    # Mercy Otieno, mercy@pngme.demo.com, 254123456789
+    user_uuid = "958a5ae8-f3a3-41d5-ae48-177fdc19e3f4"
     now = datetime(2021, 10, 1)
     now_less_30 = now - timedelta(days=30)
     now_less_60 = now - timedelta(days=60)
     now_less_90 = now - timedelta(days=90)
 
-    count_insufficient_fund_events_0_30 = count_insufficient_fund_events(client, USER_UUID, now_less_30, now)
-    count_insufficient_fund_events_31_60 = count_insufficient_fund_events(client, USER_UUID, now_less_60, now_less_30)
-    count_insufficient_fund_events_61_90 = count_insufficient_fund_events(client, USER_UUID, now_less_90, now_less_60)
+    token = os.environ["PNGME_TOKEN"]
+    client = Client(token)
+
+    count_insufficient_fund_events_0_30 = count_insufficient_funds_events(
+        client, user_uuid, now_less_30, now
+    )
+    count_insufficient_fund_events_31_60 = count_insufficient_funds_events(
+        client, user_uuid, now_less_60, now_less_30
+    )
+    count_insufficient_fund_events_61_90 = count_insufficient_funds_events(
+        client, user_uuid, now_less_90, now_less_60
+    )
