d98a586fb47c5501b781c32693a4a2f12fe08363
Kevin Ding <kevin.ding@pngme.com>
2022-03-03 11:55:07 -0800

    Switch to return None if no transaction data

--- lib/net_cash_flow/get_net_cash_flow.py
+++ lib/net_cash_flow/get_net_cash_flow.py
@@ -6,6 +6,7 @@ Typical date ranges are last 30 days, 31-60 days and 61-90 days.
 """
 from datetime import datetime
 from datetime import timedelta
+from typing import Optional
 
 import os
 from pngme.api import Client
@@ -14,7 +15,7 @@ import pandas as pd
 
 def get_net_cash_flow(
     api_client: Client, user_uuid: str, utc_starttime: datetime, utc_endtime: datetime
-) -> float:
+) -> Optional[float]:
     """Compute the net cash flow across all depository accounts for a given user in a given period.
     No currency conversions are performed.
 
@@ -25,6 +26,7 @@ def get_net_cash_flow(
         utc_endtime: the datetime for the right-hand-side of the time-window
     Returns:
         the sum total of all credit transaction amounts
+        None if there is no transaction data
     """
     account_details = api_client.accounts.get(user_uuid=user_uuid)
 
@@ -39,24 +41,22 @@ def get_net_cash_flow(
         )
         record_list.extend([dict(transaction) for transaction in transactions])
 
-    # if no data available for the user, assume cash-in is zero
+    # if no data available for the user, return None
     if len(record_list) == 0:
-        return 0.0
+        return None
 
     record_df = pd.DataFrame(record_list)
 
     # Get the total inbound credit over a period
     inflow_amount = record_df[
         (record_df.impact == "CREDIT")  # Subset by credit
-        & (
-            record_df.account_type.isin(["depository"])
-        )  # Only consider depository account
+        & (record_df.account_type == "depository")  # Only consider depository account
     ].amount.sum()
 
     # Get the total outbound debit over a period
     outflow_amount = record_df[
         (record_df.impact == "DEBIT")  # Subset by debit
-        & (record_df.account_type.isin(["depository"]))  # No loan related account
+        & (record_df.account_type == "depository")  # No loan related account
     ].amount.sum()
 
     total_net_cash_flow = inflow_amount - outflow_amount
@@ -65,7 +65,8 @@ def get_net_cash_flow(
 
 if __name__ == "__main__":
 
-    USER_UUID = "958a5ae8-f3a3-41d5-ae48-177fdc19e3f4"  # Mercy Otieno, mercy@pngme.demo.com, 254123456789
+    # Mercy Otieno, mercy@pngme.demo.com, 254123456789
+    USER_UUID = "958a5ae8-f3a3-41d5-ae48-177fdc19e3f4"
 
     client = Client(access_token=os.environ["PNGME_TOKEN"])
 
