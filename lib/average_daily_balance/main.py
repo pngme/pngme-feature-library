db8a7b64626fd8bddecfc27890159a902d807fcf
kevin-pngme <kevin@pngme.com>
2022-03-10 12:07:05 -0800

    refactor

--- lib/average_daily_balance/main.py
+++ lib/average_daily_balance/main.py
@@ -6,122 +6,79 @@ import pandas as pd
 from pngme.api import Client
 
 
-def get_institution_balances(client: Client, user_uuid: str) -> dict:
-    """Get all balances for accounts of type 'depository'.
+def get_avg_daily_balance(
+    client: Client, user_uuid: str, utc_starttime: datetime, utc_endtime: datetime
+) -> float:
+    """Get all balances for accounts of type 'depository' or 'mobilemoney'
        Returns a dictionary where the key is the insitution_uuid and 
        the value is a dataframe of balances
     Args:
-        user_uuid: user identifier
+        user_uuid (str): user identifier
     Returns:
-        account_balances: <acct_uuid, balances(pd.DataFrame)>
+        account_balances (dict): <acct_uuid, balances(pd.DataFrame)>
     """
     institutions = client.institutions.get(user_uuid)
-    institution_balances = dict()
+    institution_balances = []
 
     today = datetime.today()
-    utc_endtime = datetime(today.year, today.month, today.day)
-    utc_starttime = utc_endtime - timedelta(days=1e5)  # get all pages
+    all_pages_utc_endtime = datetime(today.year, today.month, today.day)
+    all_pages_starttime = all_pages_utc_endtime - timedelta(days=1e5)  # get all pages
 
     for institution in institutions:
         institution_id = institution.institution_id
         balances = client.balances.get(
             user_uuid=user_uuid,
             institution_id=institution_id,
-            utc_starttime=utc_starttime,
-            utc_endtime=utc_endtime,
+            utc_starttime=all_pages_starttime,
+            utc_endtime=all_pages_utc_endtime,
         )
         # convert to records
         balances = [
             balance.dict()
             for balance in balances
-            if balance.account_type == "depository"
+            if (balance.account_type == "depository")
         ]
         balances = pd.DataFrame.from_records(balances)
-        institution_balances[institution_id] = balances
+        balances = balances.assign(institution_name=institution_id)
+        institution_balances.append(balances)
 
-    return institution_balances
+    # 1. Create DF of institution balances
+    institution_balances = pd.concat(institution_balances)
+    if institution_balances.empty:
+        return None
 
+    # 2. Sort and create a column for day, filter by time window
+    institution_balances = institution_balances.sort_values("ts")
+    institution_balances["yyyymmdd"] = institution_balances["ts"].apply(
+        lambda epoch_time: datetime.fromtimestamp(epoch_time)
+    )
+    institution_balances["yyyymmdd"] = institution_balances["yyyymmdd"].dt.round("D")
 
-def _init_eod_sheet(start_ts: int, end_ts: int) -> pd.DataFrame:
-    """Get a blank balance sheet
-        Initialize a DataFrame with daily frequency and data for columns
-        'end_of_day', 'balance' and 'ts'
+    # 3. Filter time window
+    institution_balances = institution_balances[
+        (institution_balances["yyyymmdd"] >= utc_starttime)
+        & (institution_balances["yyyymmdd"] <= utc_endtime)
+    ]
+    if institution_balances.empty:
+        return None
 
-    Args:
-        start_ts: Starting timestamp of the observation window in seconds
-        end_ts: Ending timestamp of the observation window in seconds
+    # 4. Get end of day balances, if an account changes balances three times a day ($100, $20, $120), take the last one ($120)
+    eod_balances = institution_balances.groupby(
+        ["yyyymmdd", "account_number", "institution_name"]
+    ).tail(1)
+    eod_balances = eod_balances.set_index("yyyymmdd")
 
-    Returns: pd.DataFrame
-        expected columns:
-            - ts: Timestamp
-            - end_of_day: Filled with True value
-            - balance: Filled with None
-
-    """
-    SECONDS_IN_ONE_DAY = 3600 * 24
-    days = range(start_ts, end_ts + 1, SECONDS_IN_ONE_DAY)
-    eod_sheet = pd.DataFrame(days, columns=["ts"])
-    eod_sheet["end_of_day"] = True
-    eod_sheet["balance"] = None
-
-    return eod_sheet
-
-
-def get_eod_balance(
-    account_uuid: str, balances: pd.Series, start_ts: int, end_ts: int
-) -> pd.Series:
-    """Construct the end of day balance sheet
-
-    Args:
-        account_uuid
-        balance_sheet: DataFrame contains balance information
-        start_ts: Starting timestamp of the observation window in seconds
-        end_ts: Ending timestamp of the observation window in seconds
-
-    Returns: pd.Series
-        End of day account balance sheet
-    """
-    sod_sheet = balances.copy()
-    sod_sheet["end_of_day"] = False
-
-    eod_sheet = _init_eod_sheet(start_ts, end_ts)
-
-    sod_sheet = (
-        pd.concat([eod_sheet, sod_sheet], axis=0, ignore_index=True)
-        .sort_values("ts")
-        .fillna(method="ffill")
+    # 5. Forward fill missing days. Average all balances and calculate a global sum
+    ffilled_balances = (
+        eod_balances.groupby(["institution_name", "account_number"])["balance"]
+        .resample("1D", kind="timestamp")
+        .ffill()
+    )
+    avg_daily_balance = (
+        ffilled_balances.groupby(["institution_name", "account_number"]).mean().sum()
     )
 
-    eod_balance = (
-        sod_sheet[sod_sheet["end_of_day"] == True][["ts", "balance"]]
-        .set_index("ts")
-        .rename({"balance": account_uuid}, axis=1)
-    )
-    return eod_balance
-
-
-def get_avg_daily_balance(
-    account_balances: dict, utc_starttime: datetime, utc_endtime: datetime
-) -> float:
-    """Compute the daily balance
-
-    Args:
-        account_balances: Output from get_user_account_balances
-        utc_starttime: the datetime for the left-hand-side of the time-window
-        utc_endtime: the datetime for the right-hand-side of the time-window
-
-    Returns: float or None if no data was observed
-    """
-
-    start_ts = int((utc_starttime - datetime(1970, 1, 1)).total_seconds())
-    end_ts = int((utc_endtime - datetime(1970, 1, 1)).total_seconds())
-
-    eod_balances = []
-    for institution_id, balances in account_balances.items():
-        if not balances.empty:
-            eod_balance = get_eod_balance(institution_id, balances, start_ts, end_ts)
-            eod_balances.append(eod_balance)
-    return pd.concat(eod_balances, axis=1).sum(axis=1).mean()
+    return avg_daily_balance
 
 
 if __name__ == "__main__":
@@ -131,8 +88,6 @@ if __name__ == "__main__":
 
     client = Client(token)
 
-    account_balances = get_institution_balances(client=client, user_uuid=user_uuid)
-
     # avg_daily_balance_0_30
     now = datetime(2021, 10, 1)
     now_less_30 = now - timedelta(days=30)
@@ -140,15 +95,15 @@ if __name__ == "__main__":
     now_less_90 = now - timedelta(days=90)
 
     avg_daily_balance_0_30 = get_avg_daily_balance(
-        account_balances, utc_starttime=now_less_30, utc_endtime=now
+        client, user_uuid=user_uuid, utc_starttime=now_less_30, utc_endtime=now
     )
 
     avg_daily_balance_31_60 = get_avg_daily_balance(
-        account_balances, utc_starttime=now_less_60, utc_endtime=now_less_30
+        client, user_uuid=user_uuid, utc_starttime=now_less_60, utc_endtime=now_less_30
     )
 
     avg_daily_balance_61_90 = get_avg_daily_balance(
-        account_balances, utc_starttime=now_less_60, utc_endtime=now_less_90
+        client, user_uuid=user_uuid, utc_starttime=now_less_90, utc_endtime=now_less_60
     )
 
     print(avg_daily_balance_0_30)
