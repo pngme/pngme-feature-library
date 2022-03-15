6d2ab2c253d57c41ba2a86651551b33d2d96c43f
kevin-pngme <95656612+kevindingpngme@users.noreply.github.com>
2022-03-15 12:27:57 -0700

    Rename the argument (#32)

--- lib/count_overdraft_events/main.py
+++ lib/count_overdraft_events/main.py
@@ -23,7 +23,7 @@ def get_count_overdraft_events(
     Returns:
         count of overdraft events within the given time window
     """
-    overdraft_label = "Overdraft"
+    label = "Overdraft"
 
     institutions = api_client.institutions.get(user_uuid)
 
@@ -34,7 +34,7 @@ def get_count_overdraft_events(
             institution_id=institution.institution_id,
             utc_starttime=utc_starttime,
             utc_endtime=utc_endtime,
-            labels=[overdraft_label],
+            labels=[label],
         )
         overdraft_count += len(overdraft_events)
 
