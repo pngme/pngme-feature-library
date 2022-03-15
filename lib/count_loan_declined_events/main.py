3baaba1a07481ac5b04c7090c2512c76498b02d2
Kevin Ding <kevin.ding@pngme.com>
2022-03-15 14:14:57 -0700

    Switch the time range to output non-zero results

--- lib/count_loan_declined_events/main.py
+++ lib/count_loan_declined_events/main.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
     token = os.environ["PNGME_TOKEN"]
     client = Client(token)
 
-    now = datetime(2021, 10, 1)
+    now = datetime(2021, 11, 1)
     now_less_30 = now - timedelta(days=30)
     now_less_60 = now - timedelta(days=60)
     now_less_90 = now - timedelta(days=90)
