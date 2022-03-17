872f53d0cd5e3772bd331987999cb2522a3f55f4
Kevin Ding <kevin.ding@pngme.com>
2022-03-17 11:26:04 -0700

    Add helper function to compute week-to-week sum of credit standard deviation

---
+++ lib/standard_deviation_of_week_to_week_sum_of_credits/main.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+import os
+from datetime import datetime, timedelta
+from typing import Optional
+
+import pandas as pd  # type: ignore
+from pngme.api import Client
+
+
+def get_standard_deviation_of_week_to_week_sum_of_credits(
+    api_client: Client, user_uuid: str, utc_starttime: datetime, utc_endtime: datetime
+) -> Optional[float]:
+    """Compute the standard deviations of week-to-week sum of credits
+
+    Args:
+        api_client: Pngme API client
+        user_uuid: the Pngme user_uuid for the mobile phone user
+        utc_starttime: the datetime for the left-hand-side of the time-window
+        utc_endtime: the datetime for the right-hand-side of the time-window
+
+    Returns:
+        Standard deviation of week-to-week sum of credits
+    """
+    institutions = api_client.institutions.get(user_uuid=user_uuid)
+
+    # Constructs a dataframe that contains transactions from all institutions for the user
+    record_list = []
+    for institution in institutions:
+        transactions = api_client.transactions.get(
+            user_uuid=user_uuid,
+            institution_id=institution.institution_id,
+            utc_starttime=utc_starttime,
+            utc_endtime=utc_endtime,
+        )
+        record_list.extend([dict(transaction) for transaction in transactions])
+
+    # if no data available for the user, return None
+    if len(record_list) == 0:
+        return None
+
+    record_df = pd.DataFrame(record_list)
+    credit_df = record_df[
+        (record_df.impact == "CREDIT") & (record_df.account_type == "depository")
+    ]
+
+    # if no data available for credit, return None
+    if credit_df.empty:
+        return None
+
+    credit_df["date"] = pd.to_datetime(credit_df["ts"], unit="s")
+    std = credit_df.set_index("date").resample("W")["amount"].sum().std()
+    return std
+
+
+if __name__ == "__main__":
+    # Mercy Otieno, mercy@pngme.demo.com, 254123456789
+    user_uuid = "958a5ae8-f3a3-41d5-ae48-177fdc19e3f4"
+
+    token = os.environ["PNGME_TOKEN"]
+    client = Client(token)
+
+    now = datetime(2021, 11, 1)
+    now_less_84 = now - timedelta(days=84)
+
+    standard_deviation_of_week_to_week_sum_of_credits_0_84 = (
+        get_standard_deviation_of_week_to_week_sum_of_credits(
+            api_client=client,
+            user_uuid=user_uuid,
+            utc_starttime=now_less_84,
+            utc_endtime=now,
+        )
+    )
+
+    print(standard_deviation_of_week_to_week_sum_of_credits_0_84)
---
+++ lib/standard_deviation_of_week_to_week_sum_of_credits/readme.md
@@ -0,0 +1,27 @@
+# Week to Week Sum of Credits Standard Deviation
+
+1. Create and activate a virtual environment:
+
+```bash
+python3 -m venv .venv
+source .venv/bin/activate
+```
+
+2. Install the dependencies:
+
+```bash
+pip3 install -r requirements.txt
+```
+
+3. Set the `PNGME_TOKEN` environment variable using your API token from [admin.pngme.com](https://admin.pngme.com):
+
+```bash
+export PNGME_TOKEN="eyJraWQiOiJcL3d..."
+```
+
+4. Run the example:
+
+```bash
+python3 sum_of_credits.py
+
+```
