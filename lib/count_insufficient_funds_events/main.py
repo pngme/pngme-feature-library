2ba28d69e86d745d36922c72f59f8d348a6a004a
Nick Masson <nick@pngme.com>
2022-03-23 10:20:26 -0700

    optimize to subset on relevant institutions

--- lib/count_insufficient_funds_events/main.py
+++ lib/count_insufficient_funds_events/main.py
@@ -28,6 +28,9 @@ async def get_count_insufficient_funds_events(
 
     institutions = await api_client.institutions.get(user_uuid=user_uuid)
 
+    # subset to only fetch data for institutions known to contain depository-type accounts for the user
+    institutions_w_depository = [inst for inst in institutions if "depository" in inst.account_types]
+
     utc_starttime = utc_time - timedelta(days=90)
     inst_coroutines = [
         api_client.alerts.get(
@@ -37,7 +40,7 @@ async def get_count_insufficient_funds_events(
             utc_endtime=utc_time,
             labels=[label],
         )
-        for institution in institutions
+        for institution in institutions_w_depository
     ]
 
     r = await asyncio.gather(*inst_coroutines)
