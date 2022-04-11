5d9fd2e3cc2db5e4faf05ba7fa12852ab0782fe6
Ben Fasoli <ben@pngme.com>
2022-04-11 11:17:42 -0700

    Separate fetching from feature generation logic

--- lib/net_cash_flow/main.py
+++ lib/net_cash_flow/main.py
@@ -1,15 +1,45 @@
 #!/usr/bin/env python3
 
+import asyncio
 import os
 from datetime import datetime, timedelta
-from typing import Optional
+from typing import List, Optional
 
 import pandas as pd  # type: ignore
-from pngme.api import Client
+from pngme.api import AsyncClient, Institution, TransactionRecord
+
+
+async def get_institutions(
+    api_client: AsyncClient, user_uuid: str
+) -> List[Institution]:
+    institutions = await api_client.institutions.get(user_uuid=user_uuid)
+    return institutions
+
+
+async def get_depository_transactions(
+    api_client: AsyncClient, user_uuid: str, institutions: List[Institution]
+) -> List[TransactionRecord]:
+    depository_institutions = [
+        institution
+        for institution in institutions
+        if "depository" in institution.account_types
+    ]
+    transactions = []
+    for institution in depository_institutions:
+        institution_id = institution.institution_id
+        transactions.extend(
+            await api_client.transactions.get(
+                user_uuid=user_uuid,
+                institution_id=institution_id,
+            )
+        )
+    return transactions
 
 
 def get_net_cash_flow(
-    api_client: Client, user_uuid: str, utc_starttime: datetime, utc_endtime: datetime
+    depository_transactions: List[TransactionRecord],
+    utc_starttime: datetime,
+    utc_endtime: datetime,
 ) -> Optional[float]:
     """Compute the net cash flow for a user over a given period.
 
@@ -19,39 +49,23 @@ def get_net_cash_flow(
     the given period.
 
     Args:
-        api_client: Pngme API client
-        user_uuid: the Pngme user_uuid for the mobile phone user
+        transactions: list of TransactionRecords returned by client.transactions.get()
         utc_starttime: the datetime for the left-hand-side of the time-window
         utc_endtime: the datetime for the right-hand-side of the time-window
     Returns:
         the sum total of all credit transaction amounts
         None if there is no transaction data
     """
-    institutions = api_client.institutions.get(user_uuid=user_uuid)
-
-    # subset to only fetch data for institutions known to contain depository-type accounts for the user
-    institutions_w_depository = [
-        inst for inst in institutions if "depository" in inst.account_types
-    ]
-
-    # Constructs a dataframe that contains transactions from all accounts for the user
-    record_list = []
-    for institution in institutions_w_depository:
-        institution_id = institution.institution_id
-        transactions = api_client.transactions.get(
-            user_uuid=user_uuid,
-            institution_id=institution_id,
-            utc_starttime=utc_starttime,
-            utc_endtime=utc_endtime,
-            account_types=["depository"],
-        )
-        record_list.extend([dict(transaction) for transaction in transactions])
-
-    # if no data available for the user, return None
-    if len(record_list) == 0:
+    if len(depository_transactions) == 0:
         return None
 
-    transaction_df = pd.DataFrame(record_list)
+    transaction_df = pd.DataFrame([transaction.dict() for transaction in depository_transactions])
+
+    # Filter transactions by time
+    is_within_time_window = (transaction_df.ts >= utc_starttime.timestamp()) & (
+        transaction_df.ts <= utc_endtime.timestamp()
+    )
+    transaction_df = transaction_df.loc[is_within_time_window]
 
     # Get the total cash-in (credit) amount over a period
     cash_in_amount = transaction_df[(transaction_df.impact == "CREDIT")].amount.sum()
@@ -63,12 +77,13 @@ def get_net_cash_flow(
     return total_net_cash_flow
 
 
-if __name__ == "__main__":
-    # Mercy Otieno, mercy@pngme.demo.com, 254123456789
-    user_uuid = "958a5ae8-f3a3-41d5-ae48-177fdc19e3f4"
-
-    token = os.environ["PNGME_TOKEN"]
-    client = Client(access_token=token)
+async def main(api_client: AsyncClient):
+    institutions = await get_institutions(api_client=api_client, user_uuid=user_uuid)
+    depository_transactions = await get_depository_transactions(
+        api_client=api_client,
+        user_uuid=user_uuid,
+        institutions=institutions,
+    )
 
     decision_time = datetime(2021, 10, 1)
     decision_time_less_30 = decision_time - timedelta(days=30)
@@ -76,24 +91,35 @@ if __name__ == "__main__":
     decision_time_less_90 = decision_time - timedelta(days=90)
 
     net_cash_flow_0_30 = get_net_cash_flow(
-        api_client=client,
-        user_uuid=user_uuid,
+        depository_transactions=depository_transactions,
         utc_starttime=decision_time_less_30,
         utc_endtime=decision_time,
     )
     net_cash_flow_31_60 = get_net_cash_flow(
-        api_client=client,
-        user_uuid=user_uuid,
+        depository_transactions=depository_transactions,
         utc_starttime=decision_time_less_60,
         utc_endtime=decision_time_less_30,
     )
     net_cash_flow_61_90 = get_net_cash_flow(
-        api_client=client,
-        user_uuid=user_uuid,
+        depository_transactions=depository_transactions,
         utc_starttime=decision_time_less_90,
         utc_endtime=decision_time_less_60,
     )
 
-    print(net_cash_flow_0_30)
-    print(net_cash_flow_31_60)
-    print(net_cash_flow_61_90)
+    return {
+        "net_cash_flow_0_30": net_cash_flow_0_30,
+        "net_cash_flow_31_60": net_cash_flow_31_60,
+        "net_cash_flow_61_90": net_cash_flow_61_90,
+    }
+
+
+if __name__ == "__main__":
+    # Mercy Otieno, mercy@pngme.demo.com, 254123456789
+    user_uuid = "958a5ae8-f3a3-41d5-ae48-177fdc19e3f4"
+
+    token = os.environ["PNGME_TOKEN"]
+    api_client = AsyncClient(access_token=token)
+
+    result = asyncio.run(main(api_client))
+    print(result)
+    # {'net_cash_flow_0_30': 20240, 'net_cash_flow_31_60': 21737, 'net_cash_flow_61_90': -900}
