4106a46cd0c5beb9c999fee11449d703e786e55f
Dani Ramirez <dani@ext.pngme.com>
2022-04-20 14:15:31 +0200

    PDP-1134: pandas dependency removed from count_loan_defaulted_events

--- lib/count_loan_defaulted_events/main.py
+++ lib/count_loan_defaulted_events/main.py
@@ -3,9 +3,7 @@ import asyncio
 import os
 from datetime import datetime, timedelta
 
-import pandas as pd  # type: ignore
 from typing import Tuple
-
 from pngme.api import AsyncClient
 
 
@@ -24,8 +22,6 @@ async def get_count_loan_defaulted_events(
     Returns:
         count of LoanDefaulted events within the given time window
     """
-    label = "LoanDefaulted"
-
     institutions = await api_client.institutions.get(user_uuid=user_uuid)
 
     # subset to only fetch data for institutions known to contain loan-type accounts for the user
@@ -33,42 +29,46 @@ async def get_count_loan_defaulted_events(
         inst for inst in institutions if "loan" in inst.account_types
     ]
 
+    # at most, we retrieve data from last 90 days
     utc_starttime = utc_time - timedelta(days=90)
+    
+    # API call including the label filter so only "LoanDefaulted" events are returned if they exist
     inst_coroutines = [
         api_client.alerts.get(
             user_uuid=user_uuid,
             institution_id=institution.institution_id,
             utc_starttime=utc_starttime,
             utc_endtime=utc_time,
-            labels=[label],
+            labels=["LoanDefaulted"],
         )
         for institution in institutions_w_loan
     ]
 
     r = await asyncio.gather(*inst_coroutines)
 
+    # now we aggregate the results from each institution into a single list
     record_list = []
     for inst_list in r:
         record_list.extend([dict(alert) for alert in inst_list])
 
-    # if no data available for the user, assume count of LoanDefaulted event is zero
-    if len(record_list) == 0:
-        return 0, 0, 0
-
-    record_df = pd.DataFrame(record_list)
-
+    # now we prepare the time windows for the counts
     ts_30 = (utc_time - timedelta(days=30)).timestamp()
     ts_60 = (utc_time - timedelta(days=60)).timestamp()
     ts_90 = (utc_time - timedelta(days=90)).timestamp()
 
-    filter_0_30 = record_df.ts >= ts_30
-    filter_31_60 = (record_df.ts >= ts_60) & (record_df.ts < ts_30)
-    filter_61_90 = (record_df.ts >= ts_90) & (record_df.ts < ts_60)
+    # now we count the number of events in each time window
+    count_loan_defaulted_events_0_30 = 0
+    count_loan_defaulted_events_31_60 = 0
+    count_loan_defaulted_events_61_90 = 0
 
-    count_loan_defaulted_events_0_30 = len(record_df[filter_0_30])
-    count_loan_defaulted_events_31_60 = len(record_df[filter_31_60])
-    count_loan_defaulted_events_61_90 = len(record_df[filter_61_90])
-
+    for r in record_list:
+        if r["ts"] >= ts_30:
+            count_loan_defaulted_events_0_30 += 1
+        if r["ts"] >= ts_60 and r["ts"] < ts_30:
+            count_loan_defaulted_events_31_60 += 1
+        if r["ts"] >= ts_90 and r["ts"] < ts_60:
+            count_loan_defaulted_events_61_90 += 1
+    
     return (
         count_loan_defaulted_events_0_30,
         count_loan_defaulted_events_31_60,
@@ -78,6 +78,7 @@ async def get_count_loan_defaulted_events(
 
 if __name__ == "__main__":
     # Mercy Otieno, mercy@pngme.demo.com, 254123456789
+    # Has a LoanDefaulted with toc = 1633712575 (Fri Oct 08 2021 17:02:55 GMT+0000)
     user_uuid = "958a5ae8-f3a3-41d5-ae48-177fdc19e3f4"
 
     token = os.environ["PNGME_TOKEN"]
@@ -91,6 +92,7 @@ if __name__ == "__main__":
             count_loan_defaulted_events_31_60,
             count_loan_defaulted_events_61_90,
         ) = await get_count_loan_defaulted_events(client, user_uuid, now)
+        
         print(count_loan_defaulted_events_0_30)
         print(count_loan_defaulted_events_31_60)
         print(count_loan_defaulted_events_61_90)
