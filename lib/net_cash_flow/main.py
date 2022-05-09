87d679ae80b5de82603ca1425767392146fafb43
Aditya Narendra Rao <13aditya13@gmail.com>
2022-05-09 08:36:28 -0700

    [PDP-1213] net-cash-flow feature using pngme-api's AsyncClient

--- lib/net_cash_flow/main.py
+++ lib/net_cash_flow/main.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
+import asyncio
 import os
 from datetime import datetime, timedelta
 from typing import Optional
 
-from pngme.api import Client
+from pngme.api import AsyncClient
 
 
-def get_net_cash_flow(
-    api_client: Client, user_uuid: str, utc_starttime: datetime, utc_endtime: datetime
+async def get_net_cash_flow(
+    api_client: AsyncClient, user_uuid: str, utc_starttime: datetime, utc_endtime: datetime
 ) -> Optional[float]:
     """Compute the net cash flow for a user over a given period.
 
@@ -17,16 +18,16 @@ def get_net_cash_flow(
     all of a user's depository accounts during the given period.
 
     Args:
-        api_client: Pngme API client
+        api_client: Pngme Async API client
         user_uuid: the Pngme user_uuid for the mobile phone user
-        utc_starttime: the datetime for the left-hand-side of the time-window
-        utc_endtime: the datetime for the right-hand-side of the time-window
+        utc_starttime: the UTC time to start the time window
+        utc_endtime: the UTC time to end the time window
     Returns:
         the sum total of all credit transaction amounts
         None if there is no transaction data
     """
     # STEP 1: fetch list of institutions belonging to the user
-    institutions = api_client.institutions.get(user_uuid=user_uuid)
+    institutions = await api_client.institutions.get(user_uuid=user_uuid)
 
     # subset to only fetch data for institutions known to contain depository-type accounts for the user
     institutions_w_depository = []
@@ -37,15 +38,19 @@ def get_net_cash_flow(
     # STEP 2: Loop through all transactions adding the transactions
     cash_in_amount = 0
     cash_out_amount = 0
+    inst_coroutines = []
     for institution in institutions_w_depository:
-        institution_id = institution.institution_id
-        transactions = api_client.transactions.get(
+        inst_coroutines.append(api_client.transactions.get(
             user_uuid=user_uuid,
-            institution_id=institution_id,
+            institution_id=institution.institution_id,
             utc_starttime=utc_starttime,
             utc_endtime=utc_endtime,
             account_types=["depository"],
         )
+        )
+    transactions_by_institution = await asyncio.gather(*inst_coroutines)
+
+    for transactions in transactions_by_institution:
         for transaction in transactions:
             if transaction.impact == "CREDIT":
                 cash_in_amount += transaction.amount
@@ -67,16 +72,19 @@ if __name__ == "__main__":
     user_uuid = "958a5ae8-f3a3-41d5-ae48-177fdc19e3f4"
 
     token = os.environ["PNGME_TOKEN"]
-    client = Client(access_token=token)
+    client = AsyncClient(access_token=token)
 
     utc_endtime = datetime(2021, 10, 1)
     utc_starttime = utc_endtime - timedelta(days=30)
 
-    net_cash_flow = get_net_cash_flow(
-        api_client=client,
-        user_uuid=user_uuid,
-        utc_starttime=utc_starttime,
-        utc_endtime=utc_endtime,
-    )
+    async def main():
+        net_cash_flow = await get_net_cash_flow(
+            api_client=client,
+            user_uuid=user_uuid,
+            utc_starttime=utc_starttime,
+            utc_endtime=utc_endtime,
+        )
 
-    print(net_cash_flow)
+        print(net_cash_flow)
+    
+    asyncio.run(main())
